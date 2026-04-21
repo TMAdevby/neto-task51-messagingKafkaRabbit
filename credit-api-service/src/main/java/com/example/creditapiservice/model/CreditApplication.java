03efc5aab57cabc55afd0106533079d9a9a6c16d
TMAdevby <mtipun@mail.ru>
2026-04-21 12:26:26 +0300

    credit service 1

--- credit-api-service/src/main/java/com/example/creditapiservice/model/CreditApplication.java
+++ credit-api-service/src/main/java/com/example/creditapiservice/model/CreditApplication.java
@@ -1,4 +1,40 @@
 package com.example.creditapiservice.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "credit_applications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreditApplication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private Double amount;
+    private Integer term;
+    private Double income;
+    private Double currentDebt;
+    private Integer creditRating;
+
+    @Column(nullable = false)
+    private String status = "в обработке";
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
