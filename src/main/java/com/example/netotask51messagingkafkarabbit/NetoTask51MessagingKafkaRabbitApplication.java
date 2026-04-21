e0c6e1eb73ede9010884d9a67cd2be42a917d187
TMAdevby <mtipun@mail.ru>
2026-04-21 10:57:13 +0300

    initial

---
+++ src/main/java/com/example/netotask51messagingkafkarabbit/NetoTask51MessagingKafkaRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.netotask51messagingkafkarabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetoTask51MessagingKafkaRabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetoTask51MessagingKafkaRabbitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/netotask51messagingkafkarabbit/NetoTask51MessagingKafkaRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netotask51messagingkafkarabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetoTask51MessagingKafkaRabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
