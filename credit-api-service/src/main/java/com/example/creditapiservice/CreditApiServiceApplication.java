1e98ecd11bec5ca1a356d976461797fb3c1e7da9
TMAdevby <mtipun@mail.ru>
2026-04-26 13:56:13 +0300

    final

---
+++ credit-api-service/src/main/java/com/example/creditapiservice/CreditApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditApiServiceApplication.class, args);
+    }
+
+}
